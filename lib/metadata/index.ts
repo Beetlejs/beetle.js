20921ee99f1b69f064a38e49d281e5e127244682
Umut Özel <umutozel@outlook.com>
2018-08-23 13:37:29 +0300

    experimenting

--- lib/metadata/index.ts
+++ lib/metadata/index.ts
@@ -1,74 +1,22 @@
 import { Entity } from "../types";
 import { EntityManagerBase } from "../entity-manager";
 
-export interface ValidationError {
-    message: string;
-    entity: Entity;
-    property?: string;
-    value?: any;
-}
-
-export interface EntityValidationError {
-    entity: Entity;
-    validationErrors: ValidationError[];
-}
-
-export interface ManagerValidationError extends Error {
-    entities: Entity[];
-    entitiesInError: EntityValidationError[];
-    manager: EntityManagerBase;
-}
-
-export interface Validator {
-    name: string;
-    message: string;
-    args?: any;
-}
-
-export interface PropertyValidator extends Validator {
-    validate(value: any, entity: Entity): string;
-}
-
-export interface EntityValidator extends Validator {
-    validate(entity: Entity): string;
-}
-
-interface MetadataPart {
+export interface Property {
     name: string;
     displayName?: string;
-
-    toString(): string;
-    validate(entity: Entity): ValidationError[];
 }
 
-export interface Property extends MetadataPart {
-    owner: EntityType;
-    isComplex: boolean;
-    validators: PropertyValidator[];
-}
-
-interface DataTypeBase {
+export interface DataTypeBase {
     name: string;
 
     toString(): string;
-    isValid(value: any): boolean;
     defaultValue(): any;
-    autoValue(): any;
 }
 
 export interface DataProperty extends Property {
     dataType: DataTypeBase;
-    isNullable: boolean;
-    isKeyPart: boolean;
-    // generationPattern?: enums.generationPattern;
     defaultValue: any;
     useForConcurrency: boolean;
-    relatedNavigationProperties: NavigationProperty[];
-    isEnum: boolean;
-
-    isValid(value: any): boolean;
-    handle(value: any): any;
-    getDefaultValue(): any;
 }
 
 export interface NavigationProperty extends Property {
@@ -82,7 +30,7 @@ export interface NavigationProperty extends Property {
     inverse?: NavigationProperty;
 }
 
-export interface EntityType extends MetadataPart {
+export interface EntityType extends Property {
     dataProperties: DataProperty[];
     navigationProperties: NavigationProperty[];
     shortName: string;
@@ -92,10 +40,10 @@ export interface EntityType extends MetadataPart {
     properties: string[];
     floorType: EntityType;
     baseType: EntityType;
-    validators: EntityValidator[];
 
     create();
 }
 
 export class MetadataManager {
+    types: EntityType[];
 }
