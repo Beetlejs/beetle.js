fa8ec42ed66a01835bff112382fc9d64e2649049
Umut Özel <umutozel@outlook.com>
2018-08-30 21:07:56 +0300

    experimenting

--- lib/beetle/beetle-context.ts
+++ lib/beetle/beetle-context.ts
@@ -1,7 +1,6 @@
 import { Context } from "../context";
-import { IEntity, QueryParameter, WebRequestOptions, IAjaxProvider } from "../types";
+import { IEntity, QueryParameter, IAjaxProvider } from "../types";
 import { BeetleQueryOptions } from "./beetle-query-options";
-import { IRequestProvider } from "../types";
 import { MetadataManager } from "../metadata";
 import { EntitySet } from "./entity-set";
 import { MergeStrategy } from "../tracking/merge-strategy";
@@ -34,21 +33,24 @@ export class BeetleContext extends Context {
     }
 
     private mergeOptions(params: QueryParameter[], options: BeetleQueryOptions[]): BeetleQueryOptions {
+        params = params || [];
         let opt: BeetleQueryOptions = {};
         let body = {};
 
-        options.forEach(o => {
-            if (o.params) {
-                params = params.concat(o.params);
-            }
+        if (options) {
+            options.forEach(o => {
+                if (o.params) {
+                    params = params.concat(o.params);
+                }
 
-            if (o.body) {
-                body = Object.assign(body, o.body);
-            }
+                if (o.body) {
+                    body = Object.assign(body, o.body);
+                }
 
-            opt = Object.assign(opt, o);
-        });
-        
+                opt = Object.assign(opt, o);
+            });
+        }
+
         opt.params = params;
         opt.body = body;
 
