d7f6d24b2442a1b9ba908bda1625b2b33691407f
Umut Özel <umutozel@outlook.com>
2018-09-06 13:46:42 +0300

    delete jinqu-ajax test codes

--- lib/jinqu-ajax/ajax-query.ts
+++
@@ -1,9 +0,0 @@
-import { QueryPart, Query, PartArgument } from "jinqu";
-import { AjaxFuncs, AjaxOptions } from './types';
-
-export class AjaxQuery<T, TOptions extends AjaxOptions = AjaxOptions> extends Query<T> {
-
-    withOptions(options: TOptions) {
-        return this.create(QueryPart.create(AjaxFuncs.options, [PartArgument.literal(options)]));
-    }
-}
